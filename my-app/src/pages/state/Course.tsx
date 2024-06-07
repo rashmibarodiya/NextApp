4798d2d8d724f82d7f571e347139e90cc0ee798b
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-06-07 12:49:14 +0000

    state added

---
+++ my-app/src/pages/state/Course.tsx
@@ -0,0 +1,40 @@
+import { atom } from "recoil";
+
+export interface CourseType{
+    title: string,
+    description: string,
+    imageLink: string,
+    price: number,
+    published: boolean
+}
+
+export const courseState = atom<CourseType[]>({
+    key: 'courseState',
+    default: [],
+  });
+  
+
+export const courseDetails = atom<CourseType | null>({
+    key: 'courseDetails',
+    default: null
+});
+
+export const title = atom<string>({
+    key: 'title',
+    default: ""
+});
+
+export const des = atom<string>({
+    key: 'desc',
+    default: ""
+});
+
+export const img = atom<string>({
+    key: 'img',
+    default: ""
+});
+
+export const price = atom<number>({
+    key: 'price',
+    default: 0  // Correctly initialize with a number
+});
